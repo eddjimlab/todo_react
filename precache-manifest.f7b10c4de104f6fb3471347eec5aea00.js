8b4d9c240cd9e1b6d5b3a37f8b6b0e11e50f6d25
Eduard <eddjimlab@gmail.com>
2019-01-23 22:41:38 +0300

    Updates

---
+++ precache-manifest.f7b10c4de104f6fb3471347eec5aea00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "657a331f25867b90a62f",
+    "url": "/todo_react/static/js/runtime~main.657a331f.js"
+  },
+  {
+    "revision": "f61f58e95cdfcaea6787",
+    "url": "/todo_react/static/js/main.f61f58e9.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/todo_react/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "f61f58e95cdfcaea6787",
+    "url": "/todo_react/static/css/main.29799780.chunk.css"
+  },
+  {
+    "revision": "d1cabb097309b5b39aa1a83d7ef5bcde",
+    "url": "/todo_react/index.html"
+  }
+];
